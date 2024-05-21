2d7b0abbced869d4cad551165d3b57e1d4c43402
Fil <Pinskiy-FS@yandex.ru>
2024-05-21 20:00:25 +0300

    react-temlate

---
+++ src/module.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import js from './img/JS.jpg'
+
+const Module = () =>(
+	<>
+		<h1>Hello, React!</h1>
+		<img width={500} src={js} />
+	</>
+);
+
+
+export default Module;
